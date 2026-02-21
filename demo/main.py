f1ce158653b4cd074e1a21ed8ed086ade016c3e8
phantom9702 <phantom9702@gmail.com>
2026-02-21 12:58:16 +0100

    initial commit

---
+++ demo/main.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+
+
+file_path = [Path("sample1.txt"), Path("sample2.txt")]
+
+
+for i in range(0, len(file_path)):
+    if file_path[i].exists():    
+        print(file_path[i].read_text())
